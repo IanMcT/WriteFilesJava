f07976c0c1a747ad6b2d90a2855139506a1e221d
IanMcT <ian.mctavish@tldsb.on.ca>
2022-10-25 18:54:16 +0000

    Updated to add new lines.

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
 import java.io.FileWriter;
+import java.util.Scanner;
 import java.io.IOException;
+import java.time.LocalDate; // import the LocalDate class
 class Main {
   public static void main(String[] args) {
      try{
-       FileWriter file = new FileWriter("filename.txt");
-       file.write("Here is my file.\n\nNew line!");
+       Scanner input = new Scanner(System.in);
+       FileWriter file = new FileWriter("filename.txt", true);
+       String text = input.nextLine();
+       file.write(LocalDate.now()+" " + text + "\n");
        file.close();
      }catch(IOException e)
        {
